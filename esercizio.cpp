d72bdfc63fd13af3fda0f6fdd6f4af24d75a852c
valerio <valerioamato@hotmail.it>
2023-03-06 14:48:45 +0100

    da sistemare sistemare (1 funzione)

---
+++ esercizio.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int* aggiungiInFondo(int base[], int nuovo, int lunghezza){
+int newLung = lunghezza + nuovo;
+int nuovoArray[newLung];
+int i = 0;
+    for(int i;i < newLung-1; i ++){
+            base[i] = nuovoArray[i];
+            cout << nuovoArray;   
+
+    }
+        i++;
+         nuovoArray[i] = nuovo;
+         return nuovoArray;
+}
+
+int* aggiungiInTesta (int base[])
+
+
+
+int main(){
+
+
+int* aggiungiInFondo(int base[], int nuovo, int lunghezza){
+
+int* aggiungiInTesta()
+
+
+    
+}    //cambia parametri
+
+
+
+}
+
+
