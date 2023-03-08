c0f97d3d1d74c2e280f862239706ff79b1d59288
valerio <valerioamato@hotmail.it>
2023-03-08 10:27:10 +0100

    da correggere

--- esercizioSpiegazionePuntatoriMemoria.cpp
+++ esercizioSpiegazionePuntatoriMemoria.cpp
@@ -4,13 +4,13 @@ using namespace std;
  
 int main(){
  
-    int a=10;
+    int a=10;   //assegno valori
     int b=20;
  
-    cout << "a: " << a << endl;
+    cout << "a: " << a << endl;  
     cout << "b: " << b << endl;
  
-    int* p = &a;
+    int* p = &a;  //assegno al puntatore p l'indirizzo di memoria di A.
     int* q = &b;
  
     cout << "p: " << p << endl;
